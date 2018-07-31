1126b6a9414a7e58fee4be3cf74e336a3da82bb3
Andrius Skerla <andy@splytech.io>
2018-07-31 17:23:55 +0100

    bug fixes and improvements

--- src/currency.spec.ts
+++ src/currency.spec.ts
@@ -54,6 +54,7 @@ describe('currency', () => {
       'UYU', 'SAR', 'EGP', 'PKR', 'JOD', 'BHD', 'QAR',
       'LBP', 'CNY', 'MYR', 'TRY', 'KWD', 'MAD', 'SGD',
       'IDR', 'THB', 'VND', 'PHP', 'MMK', 'KHR', 'IQD',
+      'ILS', 'JPY', 'RUB', 'HKD',
     ];
     expect(Currency.getCurrencyCodes()).to.deep.equal(expectedCodes);
   });
